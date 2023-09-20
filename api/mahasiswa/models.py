eb984a9b4d05c903ec3e69d1b27e75e8300fcae8
rafimuhm_ <balurana.baau@gmail.com>
2023-09-20 10:56:21 +0700

    Membuat model mahasiswa

--- api/mahasiswa/models.py
+++ api/mahasiswa/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 # Create your models here.
-# class Mahasiswa(models.Model):
+class Mahasiswa(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    nama = models.CharField(max_length=255)
+    nim = models.CharField(max_length=10, unique=True)
+    tanggal_lahir = models.DateField()
+    jenis_kelamin = models.CharField(max_length=10)
+    alamat = models.TextField()
+    nomor_telepon = models.CharField(max_length=15)
+    email = models.EmailField()
+    status_mahasiswa = models.CharField(max_length=10)
+
+    # def __str__(self):
+    #     return self.nama
+    class Meta:
+        ordering = ['created']
